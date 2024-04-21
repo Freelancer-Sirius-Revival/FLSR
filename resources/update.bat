4748ca1fdd41116a7749e8a232eed7c453854419
Erik R <erik@erikszeug.de>
2024-04-21 19:29:39 +0200

    Update texts from "Freelancer Text Strings Revision"

--- resources/update.bat
+++ resources/update.bat
@@ -1,4 +1,6 @@
 ResourcesParser.exe
 cd frc
+frc ../resources.frc ../../EXE/resources.dll
+frc ../offerbriberesources.frc ../../EXE/offerbriberesources.dll
 frc -c ../FLSR-Texts.frc ../../EXE/FLSR-Texts.dll
 frc -c ../FLSR-Dialogs.frc ../../EXE/FLSR-Dialogs.dll

cf2cf79b021bc31b7b80a1523fb161acb5a0853f
Erik R <erik@erikszeug.de>
2024-05-24 23:35:43 +0200

    Correct referenced FLSR.frc for resource update.bat

--- resources/update.bat
+++ resources/update.bat
@@ -2,5 +2,5 @@ ResourcesParser.exe
 cd frc
 frc ../resources.frc ../../EXE/resources.dll
 frc ../offerbriberesources.frc ../../EXE/offerbriberesources.dll
-frc -c ../FLSR-Texts.frc ../../EXE/FLSR-Texts.dll
+frc -c ../FLSR.frc ../../EXE/FLSR-Texts.dll
 frc -c ../FLSR-Dialogs.frc ../../EXE/FLSR-Dialogs.dll

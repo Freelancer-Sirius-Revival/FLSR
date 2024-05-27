ced0cb3988107fd11202f1fc4b70c95ea1d406ac
Erik R <erik@erikszeug.de>
2024-05-28 01:57:57 +0200

    Move frc.exe to text resources directory

--- resources/update.bat
+++
@@ -1,6 +0,0 @@
-ResourcesParser.exe
-cd frc
-frc ../resources.frc ../../EXE/resources.dll
-frc ../offerbriberesources.frc ../../EXE/offerbriberesources.dll
-frc -c ../FLSR.frc ../../EXE/FLSR-Texts.dll
-frc -c ../FLSR-Dialogs.frc ../../EXE/FLSR-Dialogs.dll
---
+++ textResources/update.bat
@@ -0,0 +1,5 @@
+ResourcesParser.exe
+frc resources.frc ../EXE/resources.dll
+frc offerbriberesources.frc ../EXE/offerbriberesources.dll
+frc -c FLSR.frc ../EXE/FLSR-Texts.dll
+frc -c FLSR-Dialogs.frc ../EXE/FLSR-Dialogs.dll

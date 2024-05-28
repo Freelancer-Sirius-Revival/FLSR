cc41857b70034ea13684de8618b03246621b06b7
Erik R <erik@erikszeug.de>
2024-05-29 00:46:05 +0200

    Add updated Freelancer and FL Server patches and version numbers

--- patches/patch.bat
+++ patches/patch.bat
@@ -3,6 +3,7 @@ xcopy originals\*.* .\ /s /y
 bwpatchw.exe -f DLLS/BIN/Content.dll.patch
 
 bwpatchw.exe -f EXE/ebueula.dll.patch
+bwpatchw.exe -f EXE/FLServer.exe.patch
 bwpatchw.exe -f EXE/Freelancer.exe.patch
 bwpatchw.exe -f EXE/rendcomp.dll.patch
 bwpatchw.exe -f EXE/rp8.dll.patch
@@ -10,3 +11,4 @@ bwpatchw.exe -f EXE/server.dll.patch
 
 xcopy .\DLLS\*.dll ..\DLLS /s /y
 xcopy .\EXE\*.dll ..\EXE /s /y
+xcopy .\EXE\*.exe ..\EXE /s /y

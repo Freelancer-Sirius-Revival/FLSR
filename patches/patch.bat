22932f78bbd5d6d1a59c60cf3b701eae57bbb3eb
Erik R <erik@erikszeug.de>
2024-05-29 00:24:46 +0200

    Add patch for ebueula

--- patches/patch.bat
+++ patches/patch.bat
@@ -1,6 +1,8 @@
 xcopy originals\*.* .\ /s /y
 
 bwpatchw.exe -f DLLS/BIN/Content.dll.patch
+
+bwpatchw.exe -f EXE/ebueula.dll.patch
 bwpatchw.exe -f EXE/Freelancer.exe.patch
 bwpatchw.exe -f EXE/rendcomp.dll.patch
 bwpatchw.exe -f EXE/rp8.dll.patch

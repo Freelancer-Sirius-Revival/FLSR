7d5eb43c84c3cbaa0e682c44a2a139b162481e75
Erik R <erik@erikszeug.de>
2024-09-25 10:00:08 +0200

    Correct Death Penality code Damage Cause

--- 2.0/plugins/death_penalty_plugin/Main.cpp
+++ 2.0/plugins/death_penalty_plugin/Main.cpp
@@ -542,7 +542,7 @@ void __stdcall ShipDestroyed(DamageList *_dmg, DWORD *ecx, uint iKill) {
         uint iKillerID = 0;
         if (iClientID) {
             DamageList dmg;
-            if (dmg.get_cause() != DamageCause::Unknown)
+            if (dmg.get_cause() != 0x0)
                 dmg = ClientInfo[iClientID].dmgLast;
             uint iClientIDKiller = HkGetClientIDByShip(dmg.get_inflictor_id());
             if (iClientIDKiller && (iClientID != iClientIDKiller)) {

f88ec6f8a6ff64382ed945745c9822b08498f3cd
dd84ai <dd84ai@gmail.com>
2024-12-29 08:49:37 +0100

    ci: setup simple testing

---
+++ autopatch.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/darklab8/fl-data-discovery/autopatcher"
+	"github.com/darklab8/go-utils/utils/utils_os"
+)
+
+func main() {
+	// var patch autopatcher.Patch = autopatcher.Patch{}
+	freelancer_folder := autopatcher.ScanCaseInsensitiveFS(".")
+	freelancer_folder_path := utils_os.GetCurrentFolder()
+	patch_folder_path := freelancer_folder_path.Dir().Join("Freelancer")
+	patch_folder := autopatcher.ScanCaseInsensitiveFS(patch_folder_path.ToString())
+
+	for _, file := range patch_folder.Files {
+
+		content, err := os.ReadFile(file.GetPath())
+		if err != nil {
+			panic(fmt.Sprintln("failed to read file", err))
+		}
+
+		relative_patch_filepath := file.GetRelPathTo(patch_folder_path.ToString())
+
+		if strings.Contains(relative_patch_filepath, ".gitignore") {
+			continue
+		}
+
+		if freelancer_path, file_exists := freelancer_folder.LowerMapFiles[strings.ToLower(relative_patch_filepath)]; file_exists {
+			os.Remove(freelancer_path.GetPath())
+		}
+
+		relative_patch_filepath = autopatcher.AdjustFoldersInPath(relative_patch_filepath, freelancer_folder)
+
+		autopatcher.WriteToFile(relative_patch_filepath, content)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module FLSR
+
+go 1.23.4
+
+require (
+	github.com/darklab8/fl-data-discovery v0.3.0
+	github.com/darklab8/go-utils v0.21.0
+)
+
+require (
+	github.com/darklab8/go-typelog v0.6.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)

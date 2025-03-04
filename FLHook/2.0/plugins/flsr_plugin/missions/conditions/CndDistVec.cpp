e8a092bef15abe24e4bcb6b38d67634e2d262eee
Erik R <erik@erikszeug.de>
2025-03-04 03:12:46 +0100

    Correct CndDistVec matching

--- FLHook/2.0/plugins/flsr_plugin/missions/conditions/CndDistVec.cpp
+++ FLHook/2.0/plugins/flsr_plugin/missions/conditions/CndDistVec.cpp
@@ -21,8 +21,6 @@ namespace Missions
 
 	static bool isInside(const DistVecMatchEntry& entry, const CndDistVecArchetypePtr& archetype)
 	{
-		if (entry.systemId != archetype->systemId)
-			return false;
 		const bool inside = archetype->distance - HkDistance3D(archetype->position, entry.position) > 0.0f;
 		return (archetype->type == DistanceCondition::Inside && inside) || (archetype->type == DistanceCondition::Outside && !inside);
 	}
@@ -30,24 +28,50 @@ namespace Missions
 	bool CndDistVec::Matches(const std::unordered_map<uint, DistVecMatchEntry>& clientsByClientId, const std::unordered_map<uint, DistVecMatchEntry>& objectsByObjId)
 	{
 		const std::wstring outputPretext = stows(trigger->mission->archetype->name) + L"->" + stows(trigger->archetype->name) + L": Cnd_DistVec " + std::to_wstring(archetype->objNameOrLabel);
-		for (const uint clientId : trigger->mission->clientIds)
+
+		std::unordered_set<uint> validClientIds;
+		std::unordered_set<uint> validObjIds;
+		bool strangerRequested = archetype->objNameOrLabel == Stranger;
+		if (strangerRequested)
 		{
-			if (const auto& entry = clientsByClientId.find(clientId); entry != clientsByClientId.end() && isInside(entry->second, archetype))
+			validClientIds = trigger->mission->clientIds;
+		}
+		else if (const auto& objectByName = trigger->mission->objectIdsByName.find(archetype->objNameOrLabel); objectByName != trigger->mission->objectIdsByName.end())
+		{
+			validObjIds.insert(objectByName->second);
+		}
+		else if (const auto& objectsByLabel = trigger->mission->objectsByLabel.find(archetype->objNameOrLabel); objectsByLabel != trigger->mission->objectsByLabel.end())
+		{
+			for (const auto& object : objectsByLabel->second)
+			{
+				if (object.type == MissionObjectType::Client)
+					validClientIds.insert(object.id);
+				else
+					validObjIds.insert(object.id);
+			}
+		}
+		
+		for (const auto& entry : clientsByClientId)
+		{
+			if (entry.second.systemId == archetype->systemId && ((strangerRequested && !validClientIds.contains(entry.first)) || (!strangerRequested && validClientIds.contains(entry.first))) && isInside(entry.second, archetype))
 			{
 				activator.type = MissionObjectType::Client;
-				activator.id = entry->first;
+				activator.id = entry.first;
 				ConPrint(outputPretext + L" client[" + std::to_wstring(activator.id) + L"]\n");
 				return true;
 			}
 		}
-		for (const uint objId : trigger->mission->objectIds)
+		if (!strangerRequested)
 		{
-			if (const auto& entry = objectsByObjId.find(objId); entry != objectsByObjId.end() && isInside(entry->second, archetype))
+			for (const auto& entry : objectsByObjId)
 			{
-				activator.type = MissionObjectType::Object;
-				activator.id = entry->first;
-				ConPrint(outputPretext + L" client[" + std::to_wstring(activator.id) + L"]\n");
-				return true;
+				if (entry.second.systemId == archetype->systemId && validObjIds.contains(entry.first) && isInside(entry.second, archetype))
+				{
+					activator.type = MissionObjectType::Object;
+					activator.id = entry.first;
+					ConPrint(outputPretext + L" obj[" + std::to_wstring(activator.id) + L"]\n");
+					return true;
+				}
 			}
 		}
 		return false;

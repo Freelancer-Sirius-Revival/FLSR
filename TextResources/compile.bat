47101427e88661696100c8550504438d369eb2bf
Erik R <erik@erikszeug.de>
2026-01-03 17:59:08 +0100

    Add Freelancer directory input for ResourceParser

--- TextResources/compile.bat
+++ TextResources/compile.bat
@@ -1,4 +1,4 @@
-ResourcesParser.exe
+ResourcesParser.exe -i ../Freelancer/
 frc resources.frc ../Freelancer/EXE/resources.dll
 frc offerbriberesources.frc ../Freelancer/EXE/offerbriberesources.dll
 frc -c FLSR-Texts.frc ../Freelancer/EXE/FLSR-Texts.dll

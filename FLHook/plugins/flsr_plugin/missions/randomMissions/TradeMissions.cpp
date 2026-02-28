3e8977e51acac68b4ebaddbb326f3032f8f0fc72
Erik R <erik@erikszeug.de>
2026-02-28 23:31:04 +0100

    Add reputation gain to trade mission text

--- FLHook/plugins/flsr_plugin/missions/randomMissions/TradeMissions.cpp
+++ FLHook/plugins/flsr_plugin/missions/randomMissions/TradeMissions.cpp
@@ -307,6 +307,7 @@ namespace RandomMissions
 			}
 		}
 		mission.offer.description.append_fmt_str(ships);
+		mission.offer.description.append_int(reputation * 100);
 		mission.offer.reward = reward;
 		mission.offer.shipArchetypeIds = commodity.allowedShips;
 		mission.offer.baseIds = { baseId };
